5e3434081df7b7e8e7d2148e64d84f0e1bda7b3a
klin <nickles255@gmail.com>
2026-03-05 09:22:47 -0800

    feat(user): add auth token generation for user login
    - Write tests to verify token creation, bad credentials, and blank password scenarios.
    - Implement `AuthTokenSerializer` for validating user credentials.
    - Add `CreateTokenView` to generate auth tokens for authenticated users.
    - Update URLs to include a route for token generation.
    - Enable `rest_framework.authtoken` in settings.

--- app/user/views.py
+++ app/user/views.py
@@ -3,9 +3,12 @@ Views for the user API.
 """
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.serializers import (
     UsersSerializer,
+    AuthTokenSerializer,
 )
 
 
@@ -13,3 +16,10 @@ class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
 
     serializer_class = UsersSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
