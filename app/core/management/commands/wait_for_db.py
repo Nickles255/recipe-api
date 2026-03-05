22ee9a7328ba4035ad4fd8bf0d86681dadd1a091
klin <nickles255@gmail.com>
2026-03-04 22:01:00 -0800

    feat: created core application and add wait_for_db management command
    - Register the `core` app in `settings.py`.
    - Introduce `wait_for_db` command to ensure the database is available before proceeding.
    - Add tests for the new command in `test_commands`.
    - Update `docker-compose.yml` to use `wait_for_db` before running migrations and starting the server.
    - Modify GitHub workflow to include `wait_for_db` during tests.
    - Replace `psycopg2` with `psycopg2-binary` in `requirements.txt`.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,33 @@
+"""
+Django command to wait for the database to be available
+"""
+
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
+
+    def handle(self, *args, **options):
+        """Entry point for command"""
+        self.stdout.write("Waiting for database...")
+        max_tries = 10
+
+        for attempt in range(max_tries):
+            try:
+                self.check(databases=["default"])
+                self.stdout.write(self.style.SUCCESS("Database available!"))
+                return
+            except (Psycopg2Error, OperationalError):
+
+                if attempt == max_tries - 1:
+                    raise CommandError("Unable to connect to database after 10 tries")
+
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
